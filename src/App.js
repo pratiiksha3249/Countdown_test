7c7bccc01f77b53a649f8e1ecdbadeb5009badd1
pratiiksha3249 <pratikshakale7776@gmail.com>
2025-11-23 10:34:58 +0530

    first-test-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Countdown from './component/Countdown';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<Countdown></Countdown>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Countdown.jsx
@@ -0,0 +1,83 @@
+
+import { paste } from '@testing-library/user-event/dist/paste';
+import { useRef } from 'react';
+import React, { act, useEffect, useState } from 'react'
+
+const Countdown = () => {
+  const [time,setTime]=useState(0);
+  const [active,setActive]=useState(false);
+  const [pause,setPause]=useState(false);
+  const intervalRef =useRef(null);
+
+
+  //when user enter input values into input box
+  const handleInput=(event)=>{
+
+    //Minutes convert into seconds.....
+    setTime(parseInt(event.target.value*60));
+  }
+   
+  const formatTime=()=>{
+    //secounds convert into minutes...
+    const min=String(Math.floor(time/60)).padStart(2,'0');
+    const sec=String(time % 60).padStart(2,'0');
+    return `${min} : ${sec}`
+  }
+
+  const hadleStart=()=>{
+        setActive(true);
+        setPause(false);
+
+  }
+  useEffect(()=>{
+          if(active && !pause && time > 0 ){
+            intervalRef.current=setInterval(()=>{
+                 setTime((prev)=>prev-1)
+            },1000)
+          }
+          else if(time==0){
+            clearInterval(intervalRef.current);
+            setActive(false);
+            alert('Your time is up');
+          }
+          return ()=> clearInterval(intervalRef.current);
+  },[active,pause,time])
+
+  const hadlePause =()=>{
+      setPause(!pause)
+  }
+  const hadleReset=()=>{
+             clearInterval(intervalRef.current);
+             setActive(false);
+             setTime(0);
+             
+  }
+
+  return (
+    <div>
+      <center> 
+          <div>
+      <h1>CountDown</h1>
+      </div>
+
+      <div>
+        <input type='number' placeholder='Enter Time in Minute....'
+        onChange={handleInput}
+        ></input>
+      </div>
+
+      <div>
+        <h3>{formatTime()}</h3>
+      </div>
+
+      <div>
+        <button onClick={hadleStart} disabled={active && !pause}>Start</button>&nbsp;&nbsp;&nbsp;
+        <button onClick={hadlePause} disabled={!active}>{pause ? 'Resum' : 'Pause'}</button>&nbsp;&nbsp;&nbsp;
+        <button onClick={hadleReset}>Reset</button>&nbsp;&nbsp;&nbsp;
+      </div>
+</center>
+    </div>
+  )
+}
+
+export default Countdown
